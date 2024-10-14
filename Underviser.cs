a18551ba06d8d1992602aa770be3547fc0179491
Gustav <114870315+PlutoGamerpro@users.noreply.github.com>
2024-10-14 09:03:07 +0200

    Add files via upload

---
+++ Underviser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skole_Objekt_programmering_opgave
+{
+    internal class Underviser : Person
+    {
+
+        public string Navn { get; set; }
+        public DateTime Fødselsdato { get; set; }
+
+        public string Undervisingfag { get; set; }
+
+
+        public Underviser(string navn, DateTime fødselsdato, string undervisingfag)
+        {
+            Navn = navn;
+            Fødselsdato = fødselsdato;
+            Undervisingfag = undervisingfag;
+        }
+        /*
+        public override void Visinfo(List<Underviser> listUnderviser)
+        {
+            foreach (var underviser in listUnderviser)
+            {
+                Console.WriteLine($" Underviser: Navn: {underviser.Navn} :FAG: {underviser.Undervisingfag}");
+                BeregnAlder(underviser.Fødselsdato, DateTime.Now);
+            }
+        }
+        */
+
+
+
+    }
+}
